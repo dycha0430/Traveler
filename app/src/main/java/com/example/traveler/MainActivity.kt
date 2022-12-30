a95a238c817cc72b295250f46c0d01c01d48cdf6
dycha0430 <68413811+dycha0430@users.noreply.github.com>
2022-12-30 16:16:49 +0900

    test: build test

--- app/src/main/java/com/example/traveler/MainActivity.kt
+++ app/src/main/java/com/example/traveler/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.traveler
 import android.app.Activity
 import android.util.Log
 
-class MainActivity: Activity() {
+class MainActivity : Activity() {
     fun print() {
         Log.d("TAG", "Hello world")
     }
