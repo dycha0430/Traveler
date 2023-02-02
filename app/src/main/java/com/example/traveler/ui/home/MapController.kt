1a310bc792fb9eaf4bc4392a7c01946190de3190
dycha0430 <68413811+dycha0430@users.noreply.github.com>
2023-02-02 17:28:47 +0900

    Feature/gmap (#3)
    * fix: geocoder android sdk version error solved
    
    * fix: move map to specified location

--- app/src/main/java/com/example/traveler/ui/home/MapController.kt
+++ app/src/main/java/com/example/traveler/ui/home/MapController.kt
@@ -2,52 +2,81 @@ package com.example.traveler.ui.home
 
 import android.content.Context
 import android.location.Geocoder
-import android.os.Build
-import androidx.annotation.RequiresApi
+import android.widget.Toast
+import com.example.traveler.R
+import com.example.traveler.model.Destination
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
+import java.util.*
 
-class MapController constructor(context: Context) : OnMapReadyCallback {
-    private val geocoder = Geocoder(context)
-    private val zoom: Float = 8.0F
+class MapController constructor(private val context: Context, private val destination: Destination) : OnMapReadyCallback {
+    private lateinit var googleMap: GoogleMap
+    private var geocoder = Geocoder(context, Locale.getDefault())
+    private val zoom: Float = 5F
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    override fun onMapReady(googleMap: GoogleMap) {
-//        val location = getLatLngFromName("인천광역시 남동구 구월동")
-        val location = LatLng(37.413294, 127.269311)
-        if (location != null) {
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
-            googleMap.animateCamera(CameraUpdateFactory.zoomTo(zoom))
+    override fun onMapReady(gMap: GoogleMap) {
+        googleMap = gMap
+        val latLng = getLatLngFromName(destination.name)
+        if (latLng != null) {
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom*2))
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    fun getNameFromLatLng(latitude: Double, longitude: Double): String? {
+    fun moveMap(address: String) {
+        val latLng = getLatLngFromName(address)
+        if (latLng == null) {
+            Toast.makeText(context, context.getString(R.string.error_find_location), Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom*3))
+    }
+
+    private fun getNameFromLatLng(latitude: Double, longitude: Double): String? {
         var ret: String? = null
-        geocoder.getFromLocation(
-            latitude, longitude, 1,
-            Geocoder.GeocodeListener { addresses ->
-                if (addresses.isNotEmpty()) {
-                    ret = addresses[0].getAddressLine(0)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+            geocoder.getFromLocation(
+                latitude, longitude, 1,
+                Geocoder.GeocodeListener { addresses ->
+                    if (addresses.isNotEmpty()) {
+                        ret = addresses[0].getAddressLine(0)
+                    }
                 }
+            )
+        } else{
+            val list = geocoder.getFromLocation(latitude, longitude, 1)
+            if (list != null) {
+                ret = if (list.isEmpty()) null
+                else list[0].getAddressLine(0)
             }
-        )
+        }
+
         return ret
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    fun getLatLngFromName(name: String): LatLng? {
+    private fun getLatLngFromName(name: String): LatLng? {
         var ret: LatLng? = null
-        geocoder.getFromLocationName(
-            name, 1,
-            Geocoder.GeocodeListener { addresses ->
-                if (addresses.isNotEmpty()) {
-                    ret = LatLng(addresses[0].latitude, addresses[0].longitude)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+            geocoder.getFromLocationName(
+                name, 1,
+                Geocoder.GeocodeListener { addresses ->
+                    if (addresses.isNotEmpty()) {
+                        ret = LatLng(addresses[0].latitude, addresses[0].longitude)
+                    }
                 }
+            )
+        } else {
+            val list = geocoder.getFromLocationName(name, 1)
+
+            if (list != null) {
+                ret = if (list.isEmpty()) null
+                else LatLng(list[0].latitude, list[0].longitude)
             }
-        )
+        }
+
         return ret
     }
 }
